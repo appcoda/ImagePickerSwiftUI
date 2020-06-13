711603923ff5ebe12af96e56e8b27a35dacb3fb4
Simon Ng <simonng@appcoda.com>
2020-06-13 10:34:59 +0800

    Initial version

---
+++ SwiftUIImagePicker/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  SwiftUIImagePicker
+//
+//  Created by Simon Ng on 10/6/2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+    
+    var body: some View {
+        VStack {
+            
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+                        
+                    Text("Photo library")
+                        .font(.headline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+        }
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
